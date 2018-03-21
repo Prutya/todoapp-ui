dd32f25b01f88f0d1f2b6ea8fb211fe79c41dc58
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-03-21 19:51:15 +0200

    [-] Remove entry point from webpack

--- webpack.common.js
+++ webpack.common.js
@@ -2,9 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, './src/index.jsx')
-  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
