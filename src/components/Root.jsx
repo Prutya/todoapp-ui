141d676247029b5fc9e6bf20ddd094a1deb9e011
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-03-22 00:22:06 +0200

    [~] Use Browser Router

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { Router, Route } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import { TodoApp } from './TodoApp'
-
-const history = createBrowserHistory()
 
 export const Root = ({ store }) => {
   return (
     <Provider store={store}>
-      <Router history={history}>
+      <Router>
         <Route path='/' component={TodoApp} />
       </Router>
     </Provider>
