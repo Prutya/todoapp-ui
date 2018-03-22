f9b21e4c8f9690685326c7e06ef63ae8f3d43cb7
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-03-22 13:59:15 +0200

    [+] Add styling

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-
+import { render } from 'react-dom'
 import { Root } from './components/Root'
 import configureStore from './configureStore'
+import styles from './styles/application.scss'
 
 const store = configureStore()
-
-ReactDOM.render(
+render(
   <Root store={store} />,
   document.getElementById('root')
 )
