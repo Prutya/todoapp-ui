dc02076def99db9b1e595fdf8765353a8d14930c
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-03-22 23:40:03 +0200

    [~] Fix import

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import promise from 'redux-promise'
-import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import todoApp from './reducers'
 
 const configureStore = () => {
   const middlewares = [promise]
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(createLogger)
+    middlewares.push(createLogger())
   }
 
   return createStore(
