2bd18aba482ab7c6198aae9af120b4a27981d5cc
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-03-22 14:10:16 +0200

    [+] Add fake backend

---
+++ src/api/index.js
@@ -0,0 +1,37 @@
+import { v4 } from 'node-uuid'
+import { visibilityFilterConstants } from '../constants'
+
+const fakeDatabase = {
+  todos: [{
+    id: v4(),
+    text: 'Learn Ruby',
+    completed: true,
+  }, {
+    id: v4(),
+    text: 'Learn C#',
+    completed: true,
+  }, {
+    id: v4(),
+    text: 'Learn Java',
+    completed: false,
+  }],
+}
+
+const delay = (ms) => (
+  new Promise(resolve => setTimeout(resolve, ms))
+)
+
+export const fetchTodos = (filter) => (
+  delay(500).then(() => {
+    switch (filter) {
+      case visibilityFilterConstants.SHOW_ALL:
+        return fakeDatabase.todos
+      case visibilityFilterConstants.SHOW_ACTIVE:
+        return fakeDatabase.todos.filter(t => !t.completed)
+      case visibilityFilterConstants.SHOW_COMPLETED:
+        return fakeDatabase.todos.filter(t => t.completed)
+      default:
+        throw new Error(`Unknown filter: '${filter}'`)
+    }
+  })
+)
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,11 @@ import { render } from 'react-dom'
 import { Root } from './components/Root'
 import configureStore from './configureStore'
 import styles from './styles/application.scss'
+import { fetchTodos } from './api'
+
+fetchTodos('all').then(todos => {
+  console.log(todos)
+})
 
 const store = configureStore()
 render(
