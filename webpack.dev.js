91e48d6d5ea19834b11317cefcfaba8d9ed81e4a
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-03-23 22:52:37 +0200

    [~] Two separate webpack files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,57 @@
 const path = require('path');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = merge(common, {
+module.exports = {
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  resolve: {
+    alias: {},
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader'   },
+          { loader: 'sass-loader'  }
+        ]
+      },
+      {
+        test: /\.(jsx|js)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              presets: ['react', 'es2015'],
+              plugins: ['transform-object-rest-spread']
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/templates/index.html'),
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new webpack.DefinePlugin({
+      __API__: "'http://localhost:3000'"
+    })
+  ],
+
   devtool: 'eval-source-map',
 
   devServer: {
@@ -10,4 +59,4 @@ module.exports = merge(common, {
     port: 4000,
     historyApiFallback: true
   }
-});
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,54 @@
 const path = require('path');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = merge(common, {
-});
+module.exports = {
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  resolve: {
+    alias: {},
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader'   },
+          { loader: 'sass-loader'  }
+        ]
+      },
+      {
+        test: /\.(jsx|js)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              presets: ['react', 'es2015'],
+              plugins: ['transform-object-rest-spread']
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/templates/index.html'),
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new webpack.DefinePlugin({
+      __API__: "'https://todoapp-api-rails.herokuapp.com'"
+    })
+  ]
+}
