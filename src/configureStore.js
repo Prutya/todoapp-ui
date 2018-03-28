55008b3c61aaaa5c4cb7374233f0ea506007b225
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-03-28 09:28:28 +0300

    [+] Add devtools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,17 +1,16 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
 import { todoApp } from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const configureStore = () => {
-  const middlewares = [thunk]
-  if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(createLogger())
-  }
+  const middleware = [thunk]
 
   return createStore(
     todoApp,
-    applyMiddleware(...middlewares)
+    composeWithDevTools(
+      applyMiddleware(...middleware)
+    )
   )
 }
 
