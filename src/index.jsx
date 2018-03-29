77b52842375767a0be0716b8f02aad09815379b6
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-03-29 12:17:16 +0300

    Revert "[~] Move store config under the store dir"
    This reverts commit 823e59c57de165abc416278068cb0e2993e2903d.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Root from './components/Root'
-import configureStore from './store/configureStore'
+import configureStore from './configureStore'
 import styles from './styles-scss'
 
 render(
