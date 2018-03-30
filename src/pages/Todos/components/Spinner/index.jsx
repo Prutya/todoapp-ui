232839f9d874698cdc532f5e295149ef03476ce8
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-03-30 20:01:14 +0300

    [-] Remove redundant css

--- src/pages/Todos/components/Spinner/index.jsx
+++ src/pages/Todos/components/Spinner/index.jsx
@@ -2,20 +2,18 @@ import React from 'react'
 import Inner from './Inner'
 import Item from './Item'
 
-const Spinner = () => {
-  const delays = [
-      0, 100, 200,
-    200, 100,   0,
-      0, 100, 200,
-  ]
+const delays = [
+    0, 100, 200,
+  200, 100,   0,
+    0, 100, 200,
+]
 
-  return (
-    <Inner>
-      {delays.map((delay, index) =>
-        <Item key={index} delay={delay} />
-      )}
-    </Inner>
-  )
-}
+const Spinner = () => (
+  <Inner>
+    {delays.map((delay, index) =>
+      <Item key={index} delay={delay} />
+    )}
+  </Inner>
+)
 
 export default Spinner
