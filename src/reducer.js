05b82ab9ceef48368d5712c0a04fbc64031895a4
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2018-04-09 15:03:32 +0300

    [+] Reset state on logout

--- src/reducer.js
+++ src/reducer.js
@@ -2,9 +2,17 @@ import { combineReducers } from 'redux'
 import todosReducer from './pages/Todos/reducers'
 import authReducer from './pages/Auth/reducers'
 
-const root = combineReducers({
+const app = combineReducers({
   todos: todosReducer,
   auth: authReducer
 })
+
+const root = (state, action) => {
+  if (action.type === 'AUTH_SIGN_OUT') {
+    state = undefined
+  }
+
+  return app(state, action)
+}
 
 export default root
